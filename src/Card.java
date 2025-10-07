e2868ab8f51b68dc1a155fd122deb5e21210ebcb
kmanglani1 <knm205@exeter.ac.uk>
2025-10-07 15:44:58 +0000

    Card class created

---
+++ src/Card.java
@@ -0,0 +1,13 @@
+public class Card {
+
+    private final int cardValue;
+
+    public Card(int value) {
+        this.cardValue = value;
+    }
+
+    public int getCardValue() {
+        return cardValue;
+    }
+    
+}
