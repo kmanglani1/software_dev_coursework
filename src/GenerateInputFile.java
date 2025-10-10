5454583d3ee4fee5efce9f992629d81a79468824
tani-create <111594678+tani-create@users.noreply.github.com>
2025-10-10 14:55:19 +0000

    in progess

---
+++ src/GenerateInputFile.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+public class GenerateInputFile {
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        int numberOfPeople = random.nextInt(8) + 2;
+
+        System.out.println(numberOfPeople);
+    }
+}
