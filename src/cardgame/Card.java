94891ed9068feb8a34845cc5dd11de547245dc50
kmanglani1 <knm205@exeter.ac.uk>
2025-10-12 16:48:42 +0000

    CardGame input testing completed

--- src/cardgame/Card.java
+++ src/cardgame/Card.java
@@ -2,8 +2,11 @@ package cardgame;
 public class Card {
 
     private final int cardValue;
+    private final int cardId;
+    public static int idCounter = 0;
 
     public Card(int value) {
+        this.cardId = ++idCounter;
         this.cardValue = value;
     }
 
--- src/cardgame/CardGame.java
+++ src/cardgame/CardGame.java
@@ -1,39 +1,52 @@
 package cardgame;
 import java.util.Scanner;
+import java.io.File;
 
 
 
-public class GenerateInputFile {
-    public static void main(String[] args) {
+public class CardGame {
+    public static boolean validInputFile(String fileLocation, int noOfPeople) {
+        try {
+            File cardFile = new File(fileLocation);
+            
+            try (Scanner fileReader = new Scanner(cardFile)) {
+                int noOfLines = 8*noOfPeople;
+                CardDeck initialDeck = new CardDeck();
 
-        public boolean validInputFile(String fileLocation, int noOfPeople) {
-            try {
-                File cardFile = new File(fileLocation);
-                
-                try (Scanner fileReader = new Scanner(cardFile)) {
-                    int noOfLines = 8*noOfPeople
-                    CardDeck initialDeck = new CardDeck();
-
-                    for (int i = 1; i <= noOfLines; i++) {
-                        int cardValue = fileReader.nextInt();
-                        String cardName = "card"+String.valueOf(i);
-                        Card cardName = new Card(cardValue);
-                        initialDeck.addCard(cardName);
-                    }
-                } catch (Exception e) {
-                    System.out.println("Error occurred");
+                for (int i = 1; i <= noOfLines; i++) {
+                    int cardValue = fileReader.nextInt();
+                    Card card = new Card(cardValue);
+                    initialDeck.addCard(card);
                 }
+
             } catch (Exception e) {
                 System.out.println("Error occurred");
+                return false;
             }
+        } catch (Exception e) {
+            System.out.println("Error occurred");
+            return false;
         }
+        return true;
+    }
 
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter the number of people");
-        int noOfPeople = input.nextInt();
-        System.out.println("Please enter location of pack to load");
-        String fileLocation = input.nextLine();
 
+        System.out.println("Enter the number of people:");
+        int noOfPeople = input.nextInt();
+
+        input.nextLine();
+
+        System.out.println("Please enter location of pack to load:");
+        String fileLocation = input.nextLine();
+        boolean validFile = validInputFile(fileLocation, noOfPeople);
+
+        while (!validFile){
+            System.out.println("File not valid. Please enter location of pack to load:");
+            fileLocation = input.nextLine();
+            validFile = validInputFile(fileLocation, noOfPeople);
+        }
     }
 }
     
