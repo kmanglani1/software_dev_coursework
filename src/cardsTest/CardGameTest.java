9ccccc84313989f5a758715ead56d95899ddfd8a
kmanglani1 <knm205@exeter.ac.uk>
2025-10-28 13:27:20 +0000

    CardGame JUnit started

--- src/cardsTest/CardGameTest.java
+++ src/cardsTest/CardGameTest.java
@@ -1,6 +1,14 @@
 package cardgame;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+import org.junit.BeforeClass;
+import org.junit.AfterClass;
+
+
+import java.util.List;
+import java.util.ArrayList;
 
 // public class CardGameTest {
 //     @Test
